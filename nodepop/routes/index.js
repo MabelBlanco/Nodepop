b5f40a9dce722aa92e887ad12c5926cbc8f7f0b8
Mabel <cimne7@gmail.com>
2022-09-15 11:04:07 +0200

    Modifico el código para que dentro del router, los anuncios vengan de una variable

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const advertisements = [
+  {
+    name: 'Bicicleta',
+    sale: true,
+    price: 230.15, 
+    photo: 'http://localhost:3000/images/bicicleta.jpg',
+    tags: ['lifestyle', 'motor']
+  },
+  {
+    name: 'iPhone 3GS',
+    sale: false,
+    price: 50.00, 
+    photo: 'http://localhost:3000/images/iphone3gs.jpg',
+    tags: ['lifestyle', 'mobile']
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.locals.advertisements = [
-    {
-      name: 'Bicicleta',
-      sale: true,
-      price: 230.15, 
-      photo: 'http://localhost:3000/images/bicicleta.jpg',
-      tags: ['lifestyle', 'motor']
-    },
-    {
-      name: 'iPhone 3GS',
-      sale: false,
-      price: 50.00, 
-      photo: 'http://localhost:3000/images/iphone3gs.jpg',
-      tags: ['lifestyle', 'mobile']
-    }
-  ]
+  res.locals.advertisements = advertisements
 
   res.render('index', { title: 'Anuncios' });
 });
