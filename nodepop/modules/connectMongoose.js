cf79da1d87202eaa6a2b91d9c856aec7885fb8e1
Mabel <cimne7@gmail.com>
2022-11-28 21:24:34 +0100

    styles template + enviroment variables

--- nodepop/modules/connectMongoose.js
+++ nodepop/modules/connectMongoose.js
@@ -1,18 +1,18 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 // If we have an error
-mongoose.connection.on('error', error => {
-    console.log ('Ha habido un error en la conexión:', error)
-    process.exit(1)
+mongoose.connection.on("error", (error) => {
+  console.log("Ha habido un error en la conexión:", error);
+  process.exit(1);
 });
 
 // If we can connect with db
-mongoose.connection.once ('open', () => {
-    console.log ('Conectado a MongoDB en', mongoose.connection.name)
+mongoose.connection.once("open", () => {
+  console.log("Conectado a MongoDB en", mongoose.connection.name);
 });
 
 // Connect with db
-mongoose.connect ('mongodb://127.0.0.1/nodepop');
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 // Export the connection
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
